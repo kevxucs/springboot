1b6ade859ba198a5fcfdfa2d4da7c69958d910d3
Kai Xu <kevxu@my.yorku.ca>
2021-03-21 15:38:28 -0400

    add xml di demo

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import com.example.demo.bean.Customer;
+
+import org.springframework.context.ApplicationContext; 
+import org.springframework.context.support.ClassPathXmlApplicationContext; 
+
+public class Hello { 
+    public static void main(String[] args) { 
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Customer customerBean = (Customer) context.getBean("customerBean"); 
+        customerBean.displayInfo(); 
+
+        ((ClassPathXmlApplicationContext) context).close();
+    } 
+}
---
+++ src/main/java/com/example/demo/bean/Customer.java
@@ -0,0 +1,18 @@
+package com.example.demo.bean;
+
+public class Customer { 
+
+    String name; 
+
+    public String getName() { 
+        return name; 
+    }
+
+    public void setName(String name) { 
+        this.name = name; 
+    }
+
+    public void displayInfo() { 
+        System.out.println("Hello: "+ name); 
+    } 
+}

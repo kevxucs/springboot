c2235eccd5b9bb8f5cecd4919c624db133d14e10
Kai Xu <kevxu@my.yorku.ca>
2021-03-21 15:41:22 -0400

    change class name

--- src/main/java/com/example/demo/XMLDI.java
+++ src/main/java/com/example/demo/XMLDI.java
@@ -5,7 +5,7 @@ import com.example.demo.bean.Customer;
 import org.springframework.context.ApplicationContext; 
 import org.springframework.context.support.ClassPathXmlApplicationContext; 
 
-public class Hello { 
+public class XMLDI { 
     public static void main(String[] args) { 
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Customer customerBean = (Customer) context.getBean("customerBean"); 
